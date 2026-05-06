0d98cbb25a620bd78389b4b06a681189c85c04cd
Cauan S. Brandão <cauansbrandao@gmail.com>
2026-05-06 19:25:11 -0300

    feat: completar exercício 08
    Finalização da lógica pendente no exercício 08 da Lista 03. O código agora contempla todos os requisitos do enunciado e foi testado conforme os critérios de aceitação.

---
+++ lista_03_condicional/Ex_08.java
@@ -0,0 +1,42 @@
+package lista_03_condicional;
+import java.util.Scanner;
+public class Ex_08_INCOMPLETO {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num, metade1, metade2, somaMetades;
+		boolean temCaracteristica = false;
+		
+		System.out.println("================================================");
+		System.out.println("             Caracteristica do 3025");
+		System.out.println("================================================");	
+		System.out.println("Digite um numero de 4 casas para verificaçao: ");
+		System.out.print("Numero: ");
+		num = sc.nextInt();
+		
+		metade1 = num / 100;
+		metade2 = num % 100;
+		somaMetades = metade1 + metade2;
+		
+		if(somaMetades*somaMetades == num) {
+			temCaracteristica = true;
+		}
+		
+		System.out.println("\n\n\n==========================================");
+		System.out.println("          Caracteristica do 3025");
+		System.out.println("==========================================\n");	
+		System.out.println("O numero digitado foi: " + num + "\n");
+		
+		System.out.println(metade1 + " + " + metade2 + " = " + somaMetades);
+		System.out.println(somaMetades + " * " + somaMetades + " = " + somaMetades*somaMetades);
+		
+		if(!temCaracteristica) {
+			System.out.println("Logo, ele NAO tem a caracteristica do numero 3025!");
+		} else {
+			System.out.println("Logo, ele tem a caracteristica do numero 3025!");
+		}
+		
+		sc.close();
+	}
+
+}
